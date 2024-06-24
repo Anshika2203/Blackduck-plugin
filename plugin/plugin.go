ecba93a2f1de7a39dcad774a77bb41047eb24274
Anshika Anand <anshika.anand@harness.io>
2024-06-24 12:20:30 +0530

    fixes

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -14,7 +14,8 @@ import (
 type Plugin struct {
 	BlackduckURL        string `envconfig:"PLUGIN_BLACKDUCK_URL"`
 	BlackduckToken      string `envconfig:"PLUGIN_BLACKDUCK_TOKEN"`
-	BLackduckProperties string `envconfig:"PLUGIN_PROPERTIES"`
+	BlackduckProject    string `envconfig:"PLUGIN_BLACKDUCK_PROJECT"`
+	BLackduckProperties string `envconfig:"PLUGIN_BLACKDUCK_PROPERTIES"`
 	LogLevel            string `envconfig:"PLUGIN_LOG_LEVEL"`
 }
 
@@ -29,13 +30,14 @@ func (p *Plugin) Exec(ctx context.Context) error {
 func runBlackDuckScan(p *Plugin) error {
 	bdURL := p.BlackduckURL
 	bdToken := p.BlackduckToken
+	bdProject := p.BlackduckProject
 	moreProperties := p.BLackduckProperties
 
 	if bdURL == "" || bdToken == "" {
 		return fmt.Errorf("BLACKDUCK_URL and BLACKDUCK_TOKEN environment variables must be set")
 	}
 
-	command := fmt.Sprintf("bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --blackduck.url=\"%s\" --blackduck.api.token=\"%s\" --blackduck.trust.cert=true", bdURL, bdToken)
+	command := fmt.Sprintf("bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --blackduck.url=\"%s\" --blackduck.api.token=\"%s\" --detect.project.name=\"%s\" --blackduck.trust.cert=true", bdURL, bdToken, bdProject)
 
 	if moreProperties != "" {
 		command += " " + moreProperties
