cf18c8f611c918b0199500bb8815a824ad2cec0c
Anshika Anand <anshika.anand@harness.io>
2024-06-24 11:20:34 +0530

    Added plugin logic

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,1 +1,51 @@
+// Copyright 2024 the Drone Authors. All rights reserved.
+// Use of this source code is governed by the Blue Oak Model License
+// that can be found in the LICENSE file.
+
 package plugin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+type Plugin struct {
+	BlackduckURL        string `envconfig:"PLUGIN_BLACKDUCK_URL"`
+	BlackduckToken      string `envconfig:"PLUGIN_BLACKDUCK_TOKEN"`
+	BLackduckProperties string `envconfig:"PLUGIN_PROPERTIES"`
+	LogLevel            string `envconfig:"PLUGIN_LOG_LEVEL"`
+}
+
+func (p *Plugin) Exec(ctx context.Context) error {
+	if err := runBlackDuckScan(p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runBlackDuckScan(p *Plugin) error {
+	bdURL := p.BlackduckURL
+	bdToken := p.BlackduckToken
+	moreProperties := p.BLackduckProperties
+
+	if bdURL == "" || bdToken == "" {
+		return fmt.Errorf("BLACKDUCK_URL and BLACKDUCK_TOKEN environment variables must be set")
+	}
+
+	command := fmt.Sprintf("bash <(curl -s -L https://detect.synopsys.com/detect9.sh) --blackduck.url=\"%s\" --blackduck.api.token=\"%s\" --blackduck.trust.cert=true  %s", bdURL, bdToken, moreProperties)
+
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Running command: %s\n", command)
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to execute Black Duck scan command: %w", err)
+	}
+
+	return nil
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,8 @@ set -e
 set -x
 
 # linux
-GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/Blackduck-plugin
-GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/Blackduck-plugin
+GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/blackduck-plugin
+GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/blackduck-plugin
 
 # windows
-GOOS=windows go build -o release/windows/amd64/Blackduck-plugin.exe
+GOOS=windows go build -o release/windows/amd64/blackduck-plugin.exe
